9466007011ca6106dd6604a1653c1d2b6d331544
autumn-ragland <raglanda998@gmail.com>
2019-09-12 20:27:04 -0500

    Graded 09-12-2019 20:25:14

--- exercise2.js
+++ exercise2.js
@@ -1,25 +1,16 @@
+// !! : you are calling function1 which calls funtion2 which calls function1 (bad)
 let elementImage = document.getElementById("middleImage");
-
-
 let flag = false;
-
 function hideImage() {
-
     if (flag === false) {
-
         imageEvery2Seconds();
         alert("hello");
         elementImage = " ";
-
     }
 }
-
-
 function imageEvery2Seconds() {
 
     flag = true;
     let time_interval = window.setInterval(hideImage, 2000);
-
 }
-
 hideImage();
